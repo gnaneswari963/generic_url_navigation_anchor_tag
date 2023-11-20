d7410e9ce02e1d37634c0b5e9eca9c67b8bc581e
gnaneswari963 <gnaneswari22092000@gmail.com>
2023-11-20 18:28:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ammu(request):
+    return render(request,'ammu.html')
+
+def nani(request):
+    return render(request,'nani.html')
+
+def ammu1(request):
+    return render(request,'ammu1.html')
